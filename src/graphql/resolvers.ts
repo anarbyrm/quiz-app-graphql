5fa31a64dbf035fbff151d697dc207662bebbeac
Anar Bayramov <anar.bayram@yahoo.com>
2024-03-25 15:09:30 +0400

    added question query schema and resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,3 +1,14 @@
-export const resolvers = {
+import { ObjectId } from "mongoose";
+import { Answer, Question } from "../models/quiz"
 
+
+export const resolvers = {
+    Query: {
+        questions: async () => {
+            return await Question.find().populate('answers topics');
+        },
+        question: async (_: undefined, { id }: { id: ObjectId }) => {
+            return await Question.findById(id).populate('answers topics');
+        }
+    }
 }
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,3 +1,24 @@
 export const typeDefs = `#graphql
-    
+    type Query {
+        questions: [Question]!
+        question(id: ID!): Question!
+    }
+
+    type Question {
+        _id: ID!
+        body: String!
+        topics: [Topic]!
+        answers: [Answer]!
+    }
+
+    type Topic {
+        _id: ID! 
+        name: String!
+    }
+
+    type Answer {
+        _id: ID!
+        body: String!
+        isCorrect: Boolean
+    }
 `
